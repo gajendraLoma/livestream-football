185d12d7d8dc4bbedfe8c5e5d8134e3a9e30d1b8
gajendraLoma <141911581+gajendraLoma@users.noreply.github.com>
2025-01-15 08:19:32 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ const nextConfig = {
       return [
         {
           source: '/',
-          destination: '/', 
+          destination: '/home', 
           permanent: true, 
         },
       ];
